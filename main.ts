2f03531bea8d8b6ce59c534b75227b08ef2a1b28
Kai <68577140+Kaixvny@users.noreply.github.com>
2024-11-20 22:08:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,54 @@
-let has_called = 0
-let timer = 0
-let sensor_dist = 0
+function song () {
+    music.play(music.tonePlayable(554, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(415, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(831, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(740, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(554, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(415, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(831, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(740, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(554, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(415, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(831, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(740, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(554, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(415, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Heart)
+}
+function intro () {
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(466, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(415, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(370, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(349, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(311, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+}
 input.onButtonPressed(Button.A, function () {
     thing1()
     has_called = 1
@@ -23,18 +71,70 @@ function thing1 () {
         turn("left", 60)
     }
 }
-basic.forever(function () {
-    sensor_dist = maqueen.Ultrasonic()
-    if (sensor_dist < 25 && has_called == 0) {
-        thing1()
-        timer = 100
-        has_called = 1
-    } else {
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
-    }
-})
+function melody () {
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+}
+let final_over = 0
+let sensor_dist = 0
+let timer = 0
+let has_called = 0
+music.setVolume(255)
+music.setTempo(118)
+has_called = 0
+let finished = true
+radio.setGroup(1)
 basic.forever(function () {
     if (timer > 0) {
         timer += -1
+    }
+})
+basic.forever(function () {
+    sensor_dist = maqueen.Ultrasonic()
+    if (finished == false) {
+        if (sensor_dist < 25 && has_called == 0) {
+            thing1()
+            timer = 100
+            has_called = 1
+        } else {
+            Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
+        }
+    } else {
+        if (final_over == 0) {
+            intro()
+            final_over = 1
+            song()
+            radio.sendNumber(23)
+        }
     }
 })
